f03b637f49bfa634d28d233476813e52029f095a
Sairoden <odingandarosa@gmail.com>
2023-01-08 16:55:27 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -2,8 +2,38 @@ import React, { useState, useEffect } from "react";
 import data from "./data";
 import Article from "./Article";
 
+const localTheme = window.localStorage.getItem("theme");
+
 function App() {
-  return <h2>dark mode starter</h2>;
+  const [theme, setTheme] = useState(localTheme || "white-theme");
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    window.localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const handleTheme = () => {
+    if (theme === "white-theme") setTheme("dark-theme");
+    else setTheme("white-theme");
+  };
+
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={handleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map(article => (
+          <Article {...article} key={article.id} />
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default App;
